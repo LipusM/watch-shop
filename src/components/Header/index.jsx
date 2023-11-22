e063aea0f03df69f968a3e0001aa1aa068f5fdfb
LipusM <mlipus007@gmail.com>
2023-11-22 23:40:03 +0100

    ..

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,41 +1,40 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 
+import AdbIcon from "@mui/icons-material/Adb";
 
-import AdbIcon from '@mui/icons-material/Adb';
-
-const pages =[
-    {
-        page: "Home",
-        url: "/",
-    },
-    {
-        page: "Watches",
-        url: "/watches",
-    },
-    {
-        page: "About us",
-        url: "/about-us",
-    },
-    {
-        page: "Contact",
-        url: "/contact",
-    },
-]
+const pages = [
+  {
+    page: "Home",
+    url: "/",
+  },
+  {
+    page: "Watches",
+    url: "/watches",
+  },
+  {
+    page: "About us",
+    url: "/about-us",
+  },
+  {
+    page: "Contact",
+    url: "/contact",
+  },
+];
 
 import { NavLink } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 
-import "./style.scss"
+import "./style.scss";
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -43,33 +42,33 @@ const Header = () => {
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-
   return (
-        <AppBar position="static" style={{ background: "orange" }} elevation={1}>
+    <AppBar position="static" style={{ background: "orange" }} elevation={1}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
-          
             variant="h6"
             noWrap
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
+              letterSpacing: ".3rem",
             }}
           >
-              <NavLink to="/"><img src="/logo.svg" alt="logo" /></NavLink>
+            <NavLink to="/">
+              <img src="/logo.svg" alt="logo" />
+            </NavLink>
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+{/* Responzivní menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -77,58 +76,66 @@ const Header = () => {
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
             >
+              {/* <div id="nav-btn" className="first-section">
+                <div className="nav-part"></div>
+                <div className="nav-part"></div>
+                <div className="nav-part"></div>
+              </div> */}
               <MenuIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none", },
               }}
             >
               {pages.map((page) => (
                 <MenuItem key={page.page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">
                     <NavLink to={page.url}>{page.page}</NavLink>
+                    {/* {page.page} */}
                   </Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
+          {/* Responzivní menu */}
 
           <Typography
             variant="h5"
             noWrap
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
+              letterSpacing: ".3rem",
             }}
           >
-              <NavLink to="/"><img src="/logo.svg" alt="logo" /></NavLink>
+            <NavLink to="/">
+              <img src="/logo.svg" alt="logo" />
+            </NavLink>
           </Typography>
 
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page.page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 <NavLink to={page.url}>{page.page}</NavLink>
               </Button>
@@ -138,5 +145,5 @@ const Header = () => {
       </Container>
     </AppBar>
   );
-}
+};
 export default Header;
