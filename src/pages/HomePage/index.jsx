7edf347bb74b71ef4d28e3ae97cafa4df932d627
LipusM <mlipus007@gmail.com>
2023-11-28 20:28:39 +0100

    ..

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -7,7 +7,8 @@ const HomePage = () => {
   return (
     <main id="home-page">
       <motion.button
-      whileHover={{opacity: 0.8}}
+      /* whileHover={{opacity: 0.8}} */
+      whileHover={{ backgroundColor: "rgba(255, 255, 255, 0.6)"}}
       whileTap={{scale: 0.9}}
       transition={{duration: 0.5}}
       >
